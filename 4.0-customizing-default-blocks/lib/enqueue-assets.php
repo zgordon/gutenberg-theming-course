53973a417ba3417120049ab3b40132c03528ee22
Joe Casabona <jcasabona@gmail.com>
2018-08-30 14:27:03 -0400

    Add initial 4.0 folders

---
+++ 4.0-customizing-default-blocks/lib/enqueue-assets.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Gutenberg_Courses\Theming;
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_theme_assets' );
+/**
+ * Enqueue theme CSS
+ */
+function enqueue_theme_assets() {
+
+	wp_enqueue_style(
+		'parent-style',
+		get_template_directory_uri() . '/style.css'
+	);
+
+}
+
