7bcc19a511a649135476c7a1e4b4b9e643d1cb43
Joe Casabona <jcasabona@gmail.com>
2018-08-31 14:02:32 -0400

    Added code for Quotes/Pull Quotes

---
+++ 4.3-custom-block-css/functions.php
@@ -0,0 +1,19 @@
+<?php
+namespace Gutenberg_Courses\Theming;
+
+// Enqueue CSS and JS
+require_once( __DIR__ . '/lib/enqueue-assets.php' );
+
+// Add Theme Support
+require_once( __DIR__ . '/lib/theme-support.php' );
+
+add_filter( 'body_class', __NAMESPACE__ . '\add_gutenberg_compatible_body_class' );
+/**
+ * Add custom class for Gutenberg Compatible template
+ */
+function add_gutenberg_compatible_body_class( $classes ) {
+    if( is_page_template('page-full-width.php') )
+        $classes[] = 'gutenberg-compatible-template';
+    return $classes;
+}
+
