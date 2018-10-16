f4ff85063ba407189e2c802a90253d4e0f1cd300
Zac Gordon <email@zacgordon.com>
2018-10-16 16:37:41 -0400

    Switching to get_stylesheet_directory_uri for theme CSS and JS enqueues

--- 4.3-custom-block-css/lib/enqueue-assets.php
+++ 4.3-custom-block-css/lib/enqueue-assets.php
@@ -1,13 +1,3 @@
-<?php
-
-namespace Gutenberg_Courses\Theming;
-
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_theme_assets' );
-/**
- * Enqueue theme CSS
- */
-function enqueue_theme_assets() {
-
 	wp_enqueue_style(
 		'parent-style',
 		get_template_directory_uri() . '/style.css'
@@ -15,20 +5,17 @@ function enqueue_theme_assets() {
 
 	wp_enqueue_style(
 		'block-styles',
-		get_template_directory_uri() . '/blocks.css'
+		get_stylesheet_directory_uri() . '/blocks.css'
 	);
 
 	wp_enqueue_script( 
 		'fitvids-js', 
-		get_template_directory_uri(). '/lib/fitvids.jquery.js', 
+		get_stylesheet_directory_uri(). '/lib/fitvids.jquery.js', 
 		array( 'jquery' )
 	);
 
 	wp_enqueue_script( 
 		'gutenbergtheme-scripts-js', 
-		get_template_directory_uri(). '/lib/scripts.js', 
+		get_stylesheet_directory_uri(). '/lib/scripts.js', 
 		array( 'fitvids-js' )
 	);
-
-}
-
